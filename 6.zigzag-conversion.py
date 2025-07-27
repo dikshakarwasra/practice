27b6edd1ae6d6540e925659fedff4c61d123f3de
dikshakarwasra <karwasradiksha298@gmail.com>
2025-07-27 22:18:58 +0530

    longest palindromic substring

---
+++ 6.zigzag-conversion.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=6 lang=python
+#
+# [6] Zigzag Conversion
+#
+
+# @lc code=start
+class Solution(object):
+    def convert(self, s, numRows):
+        
+        
+        
+# @lc code=end
+
