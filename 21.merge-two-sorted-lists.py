7b3f13dede9515980d439eba6aae95543a3a3d30
dikshakarwasra <karwasradiksha298@gmail.com>
2025-07-27 21:30:15 +0530

    threeSum

---
+++ 21.merge-two-sorted-lists.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=21 lang=python
+#
+# [21] Merge Two Sorted Lists
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        
+       
+# @lc code=end
+
---
+++ 26.remove-duplicates-from-sorted-array.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=26 lang=python
+#
+# [26] Remove Duplicates from Sorted Array
+#
+
+# @lc code=start
+class Solution(object):
+    def removeDuplicates(self, nums):
+        
+        
+# @lc code=end
+
