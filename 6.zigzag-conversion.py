2a6929bb58f899587b2b75b7d0298922b627a34a
dikshakarwasra <karwasradiksha298@gmail.com>
2025-07-28 10:47:38 +0530

    zigzag

--- 6.zigzag-conversion.py
+++ 6.zigzag-conversion.py
@@ -5,8 +5,31 @@
 #
 
 # @lc code=start
-class Solution(object):
+class Solution:
     def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [''] * numRows
+        row = 0
+        increment = 1
+
+        for c in s:
+            rows[row] += c
+            if row == 0:
+                increment = 1
+            elif row == numRows - 1:
+                increment = -1
+            row += increment
+
+        return ''.join(rows)
+
+                  
+
+            
+
+
+
         
         
         
