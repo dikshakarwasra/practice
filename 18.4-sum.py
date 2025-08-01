9ebbad32b44963c9a326a1735effca4882afff3a
dikshakarwasra <karwasradiksha298@gmail.com>
2025-08-01 22:17:40 +0530

    4 sum

--- 18.4-sum.py
+++ 18.4-sum.py
@@ -7,6 +7,33 @@
 # @lc code=start
 class Solution(object):
     def fourSum(self, nums, target):
+        nums.sort()
+        res, quad = [], []
+
+        def kSum(k, start, target):
+            if k == 2:
+                l, r = start, len(nums) - 1
+                while l < r:
+                    if nums[l] + nums[r] < target:
+                        l += 1
+                    elif nums[l] + nums[r] > target:
+                        r -= 1
+                    else:
+                        res.append(quad + [nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l - 1]:
+                            l += 1
+            else:
+                for i in range(start, len(nums) - k + 1):
+                    if i > start and nums[i] == nums[i - 1]:
+                        continue
+                    quad.append(nums[i])
+                    kSum(k - 1, i + 1, target - nums[i])
+                    quad.pop()
+
+        kSum(4, 0, target)
+        return res
+
         
         
          
