6f34d439151816e0cfb92ed136f907673260d243
dikshakarwasra <karwasradiksha298@gmail.com>
2025-08-06 22:23:26 +0530

    index of first occ in a str

--- 28.find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.py
@@ -7,11 +7,15 @@
 # @lc code=start
 class Solution(object):
     def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
+        if needle == "":
+            return 0
+
+        for i in range(len(haystack) + 1 - len(needle)):
+            if haystack[i : i + len(needle)] == needle:
+                return i
+
+        return -1
+
         
 # @lc code=end
 
