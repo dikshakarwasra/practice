45ad38cd7a7601f396bd8c68c1c3ff807229f3c5
dikshakarwasra <karwasradiksha298@gmail.com>
2025-08-07 21:37:16 +0530

    valid sudoku

--- 36.valid-sudoku.py
+++ 36.valid-sudoku.py
@@ -7,10 +7,30 @@
 # @lc code=start
 class Solution(object):
     def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)  # key = (r // 3, c // 3)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
+
+        
+
+
+        
         
 # @lc code=end
 
