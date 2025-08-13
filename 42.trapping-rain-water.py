9919b2991c50343d42e683617660d141e0904f6e
dikshakarwasra <karwasradiksha298@gmail.com>
2025-08-13 22:50:35 +0530

    trapping rainwater

---
+++ 42.trapping-rain-water.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=42 lang=python
+#
+# [42] Trapping Rain Water
+#
+
+# @lc code=start
+class Solution(object):
+    def trap(self, height):
+        if not height: return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+
+        return res
+        
+        
+# @lc code=end
+
