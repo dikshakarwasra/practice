5a3cba092eb281220e3153f4a3b28e6089f51173
dikshakarwasra <karwasradiksha298@gmail.com>
2025-08-20 22:22:40 +0530

    search a 2D matrix

---
+++ 74.search-a-2-d-matrix.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=74 lang=python
+#
+# [74] Search a 2D Matrix
+#
+
+# @lc code=start
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        rows, cols = len(matrix), len(matrix[0])
+        top, bottom = 0, rows - 1
+
+        while top <= bottom:
+            row = (top + bottom) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bottom = row - 1
+            else:
+                break
+
+        if not (top <= bottom):
+            return False
+
+        row = (top + bottom) // 2
+        l, r = 0, cols - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+
+        return False
+        
+        
+# @lc code=end
+
