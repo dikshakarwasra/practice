88bb2f4fd782300c9f1c04d131b730492a23e478
dikshakarwasra <karwasradiksha298@gmail.com>
2025-08-20 08:24:05 +0530

    binary search

---
+++ 704.binary-search.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=704 lang=python
+#
+# [704] Binary Search
+#
+
+# @lc code=start
+class Solution(object):
+    def search(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+
+                     
+        
+        
+# @lc code=end
+
