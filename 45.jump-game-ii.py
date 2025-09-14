faea09ff3cc18d759fde3f3f41e247800939ed78
dikshakarwasra <karwasradiksha298@gmail.com>
2025-09-14 23:18:16 +0530

    jump game II

---
+++ 45.jump-game-ii.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=45 lang=python
+#
+# [45] Jump Game II
+#
+
+# @lc code=start
+class Solution(object):
+    def jump(self, nums):
+        jumps = 0
+        end = 0
+        farthest = 0
+
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+            if i == end:
+                jumps += 1
+                end = farthest
+
+        return jumps
+
+
+       
+        
+# @lc code=end
+
