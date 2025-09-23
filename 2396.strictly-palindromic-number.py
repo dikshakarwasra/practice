cd2574716452a684a446e7bd5c6863cd42ecf5ea
dikshakarwasra <karwasradiksha298@gmail.com>
2025-09-23 19:57:58 +0530

    strictly palindromic number

---
+++ 2396.strictly-palindromic-number.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=2396 lang=python
+#
+# [2396] Strictly Palindromic Number
+#
+
+# @lc code=start
+class Solution(object):
+    def isStrictlyPalindromic(self, n):
+        return False
+        
+# @lc code=end
+
