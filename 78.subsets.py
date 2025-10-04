b716a1a56618be7aaa0b6df37fe5c44607ad21dd
dikshakarwasra <karwasradiksha298@gmail.com>
2025-10-04 18:09:11 +0530

    subsets

---
+++ 78.subsets.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=78 lang=python
+#
+# [78] Subsets
+#
+
+# @lc code=start
+class Solution(object):
+    def subsets(self, nums):
+        result = []   
+
+        def backtrack(start, path):
+            result.append(path[:])
+
+            # explore further numbers
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return result
+        
+        
+# @lc code=end
+
