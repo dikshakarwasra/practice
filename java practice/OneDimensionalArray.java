ba153949a1094d7b72a79930c6cf1b18582a4b51
dikshakarwasra <karwasradiksha298@gmail.com>
2025-10-06 21:30:42 +0530

    subsets II

--- java practice/OneDimensionalArray.java
+++ java practice/OneDimensionalArray.java
@@ -1,6 +1,6 @@
 public class OneDimensionalArray{
     public static void main(String[]args){
-        int numbers[]={10,20,30,40,50};
+        int numbers[]={10,20,30,40,70};
         System.out.println("Array elements are:");
         for(int i=0;i<numbers.length;i++){
             System.out.println(numbers[i]);
---
+++ leetcode practice/53.maximum-subarray.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=53 lang=python
+#
+# [53] Maximum Subarray
+#
+
+# @lc code=start
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+# @lc code=end
+
