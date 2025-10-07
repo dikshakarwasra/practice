6fb1a95f5d760ff407d70b91f9c6a285f8cb9a74
dikshakarwasra <karwasradiksha298@gmail.com>
2025-10-07 22:03:11 +0530

    n-queens

---
+++ leetcode practice/51.n-queens.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=51 lang=python
+#
+# [51] N-Queens
+#
+
+# @lc code=start
+class Solution(object):
+    def solveNQueens(self, n):
+        col = set()
+        posDiag = set()  # (r + c)
+        negDiag = set()  # (r - c)
+
+        res = []
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+
+                backtrack(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+
+        backtrack(0)
+        return res
+       
+        
+# @lc code=end
+
