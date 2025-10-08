ba33c1c58245e5bd7b9b6b638f0da56dd031aaa5
dikshakarwasra <karwasradiksha298@gmail.com>
2025-10-08 22:50:04 +0530

    number of islands

---
+++ leetcode practice/200.number-of-islands.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=200 lang=python
+#
+# [200] Number of Islands
+#
+
+# @lc code=start
+class Solution(object):
+    def numIslands(self, grid):
+        if not grid:
+            return 0 
+        rows = len(grid)
+        cols = len(grid[0])
+        count = 0
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0':
+                return
+            
+            grid[r][c] = '0'
+            dfs(r + 1, c)  
+            dfs(r - 1, c)  
+            dfs(r, c + 1)  
+            dfs(r, c - 1)  
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1': 
+                    count += 1
+                    dfs(r, c)  
+
+        return count
+        # @lc code=end
+
