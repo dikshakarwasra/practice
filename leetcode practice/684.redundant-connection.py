26199c351f36cbab9cf775b471fcf0410f1aec3a
dikshakarwasra <karwasradiksha298@gmail.com>
2025-10-12 22:26:11 +0530

    redundant connection

---
+++ leetcode practice/684.redundant-connection.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=684 lang=python
+#
+# [684] Redundant Connection
+#
+
+# @lc code=start
+class Solution(object):
+    def findRedundantConnection(self, edges):
+        par=[i for i in range(len(edges)+1)]
+        rank=[1]* (len(edges)+1)
+
+        def find(n):
+            p=par[n]
+            while p!=par[p]:
+                par[p]=par[par[p]]
+                p=par[p]
+            return p
+        
+        def union(n1,n2):
+            p1,p2=find(n1),find(n2)
+            if p1==p2:
+                return False
+            if rank[p1]>rank[p2]:
+                par[p2]=p1
+                rank[p1]+=rank[p2]
+            else:
+                par[p1]=p2
+                rank[p2]+=rank[p1]
+            return True
+
+        for n1,n2 in edges:
+            if not union(n1,n2):
+                return[n1,n2]         
+
+
+
+
+       # @lc code=end
+
