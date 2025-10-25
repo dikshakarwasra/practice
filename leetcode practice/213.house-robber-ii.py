81b78d5fcd8ecaa45e6ca315dc37643d8d6a8e8f
dikshakarwasra <karwasradiksha298@gmail.com>
2025-10-25 21:59:41 +0530

    house robber II

---
+++ leetcode practice/213.house-robber-ii.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=213 lang=python
+#
+# [213] House Robber II
+#
+
+# @lc code=start
+class Solution(object):
+    def rob(self, nums):
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            newRob = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = newRob
+        return rob2
+        
+# @lc code=end
+
