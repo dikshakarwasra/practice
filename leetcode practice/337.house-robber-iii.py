2e1832d42562703f1778b3273b92a0c083bf5f16
dikshakarwasra <karwasradiksha298@gmail.com>
2025-10-26 12:42:41 +0530

    house robber III

---
+++ leetcode practice/337.house-robber-iii.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=337 lang=python
+#
+# [337] House Robber III
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rob(self, root):
+        def dfs(root):
+            if not root:
+                return [0, 0]
+
+            leftPair = dfs(root.left)
+            rightPair = dfs(root.right)
+
+            withRoot = root.val + leftPair[1] + rightPair[1]
+            withoutRoot = max(leftPair) + max(rightPair)
+
+            return [withRoot, withoutRoot]
+
+        return max(dfs(root))
+        
+        
+# @lc code=end
+
