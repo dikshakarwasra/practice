f3b3e1adb30e71ff0b963cba3dda341fd233bffa
dikshakarwasra <karwasradiksha298@gmail.com>
2025-11-20 22:11:57 +0530

    final value of variable after performing operations

---
+++ leetcode practice/2011.final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=2011 lang=python
+#
+# [2011] Final Value of Variable After Performing Operations
+#
+
+# @lc code=start
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        x = 0 
+
+        for op in operations:
+            if op == "++X" or op == "X++":
+                x += 1
+            else:  
+                x -= 1
+
+        return x
+
+        
+        
+# @lc code=end
+
