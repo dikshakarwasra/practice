a094799eeb3d12d520889e12311bac6d97b9fa6b
dikshakarwasra <karwasradiksha298@gmail.com>
2026-01-04 10:25:59 +0530

    spiral matrix

---
+++ leetcode practice/54.spiral-matrix.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode id=54 lang=python
+#
+# [54] Spiral Matrix
+#
+
+# @lc code=start
+class Solution(object):
+    def spiralOrder(self, matrix):
+        res = []
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+
+        while left < right and top < bottom:
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top += 1
+
+            for i in range(top, bottom):
+                res.append(matrix[i][right - 1])
+            right -= 1
+
+            if not (left < right and top < bottom):
+                break
+
+            for i in range(right - 1, left - 1, -1):
+                res.append(matrix[bottom - 1][i])
+            bottom -= 1
+
+            for i in range(bottom - 1, top - 1, -1):
+                res.append(matrix[i][left])
+            left += 1
+
+        return res
+       
+        
+# @lc code=end
+
