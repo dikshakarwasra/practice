4e0aeffe7ddf40ef31236f2c020ba3f2b8cc6508
dikshakarwasra <karwasradiksha298@gmail.com>
2026-01-07 19:48:10 +0530

    maximum subarray

--- leetcode practice/53.maximum-subarray.py
+++ leetcode practice/53.maximum-subarray.py
@@ -7,10 +7,20 @@
 # @lc code=start
 class Solution(object):
     def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        maxSub = nums[0]
+        curSum = 0
+
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+
+            curSum += n
+            maxSub = max(maxSub, curSum)
+
+        return maxSub
+       
+
+        
         
 # @lc code=end
 
