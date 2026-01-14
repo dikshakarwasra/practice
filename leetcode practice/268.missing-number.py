dc83772330b4dcb3c197bd24d0e30dad9ab9a669
dikshakarwasra <karwasradiksha298@gmail.com>
2026-01-14 11:09:56 +0530

    missing number

---
+++ leetcode practice/268.missing-number.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=268 lang=python
+#
+# [268] Missing Number
+#
+
+# @lc code=start
+class Solution(object):
+    def missingNumber(self, nums):
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
+        return len(nums)
+        
+# @lc code=end
+
