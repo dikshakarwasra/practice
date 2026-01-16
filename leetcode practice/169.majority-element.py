326c72349be843e605309c42775f945210cf1217
dikshakarwasra <karwasradiksha298@gmail.com>
2026-01-16 17:48:05 +0530

    majority element

---
+++ leetcode practice/169.majority-element.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=169 lang=python
+#
+# [169] Majority Element
+#
+
+# @lc code=start
+class Solution(object):
+    def majorityElement(self, nums):
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+
+        return candidate
+        
+        
+# @lc code=end
+
