ff0433be3e04c7a9aeaa938fb28debbd78ed1e55
dikshakarwasra <karwasradiksha298@gmail.com>
2026-01-30 22:14:49 +0530

    sort colors

---
+++ leetcode practice/75.sort-colors.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=75 lang=python
+#
+# [75] Sort Colors
+#
+
+# @lc code=start
+class Solution(object):
+    def sortColors(self, nums):
+        l, r = 0, len(nums) - 1
+        i = 0
+
+        def swap(i, j):
+            tmp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = tmp
+
+        while i <= r:
+            if nums[i] == 0:
+                swap(l, i)
+                l += 1
+            elif nums[i] == 2:
+                swap(i, r)
+                r -= 1
+                i -= 1
+            i += 1
+        
+        
+# @lc code=end
+
